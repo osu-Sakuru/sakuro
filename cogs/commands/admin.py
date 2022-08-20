cec3604ec74584907689fd6b64342263c976ebc6
alowave223 <belofost22@gmail.com>
2022-08-20 12:51:13 +0300

    removed calc, fixed rqmap

--- cogs/commands/admin.py
+++ cogs/commands/admin.py
@@ -16,7 +16,6 @@ from objects import glob
 from objects.sakuro import ContextWrap
 from objects.sakuro import Sakuro
 from objects.user import UserHelper
-from osu.calculator import Calculator
 from utils.misc import convert_grade_emoji
 from utils.misc import convert_status_str
 from utils.misc import convert_str_status
@@ -52,100 +51,6 @@ class AdminCog(commands.Cog, name="Admin"):
     async def shutdown(self, ctx: ContextWrap) -> None:
         await ctx.send("Night night..")
         await self.bot.close()
-
-    @sakuroCommand(hidden=True)
-    @commands.check(sakuru_only)
-    @commands.has_permissions(ban_members=True)
-    async def replay(self, ctx: ContextWrap, nickname: str, mods: str, map_id: int):
-        player = await UserHelper.getOsuUserByName(make_safe_name(nickname), "info")
-        description = ""
-
-        if not player:
-            async with glob.http.get(
-                "https://sakuru.pw/api/search", params={"q": nickname}
-            ) as resp:
-                if resp.status == 200:
-                    data = await resp.json()
-
-                    if data["length"] == 0:
-                        return await ctx.send(
-                            f"Nothing matched with {nickname} not found, check your spelling."
-                        )
-
-                    embed = Embed(color=ctx.author.color, timestamp=datetime.now())
-
-                    embed.set_author(name=f"Search queue for {nickname}")
-
-                    for idx, row in enumerate(data["matches"]):
-                        description += f"**{idx + 1}.** [{row['name']}](https://sakuru.pw/u/{row['id']})\n"
-
-                    embed.description = description
-                    description = ""
-                    message = await ctx.send(embed=embed)
-
-                    for emoji in QUEUE_EMOJIS[: data["length"]]:
-                        await message.add_reaction(emoji)
-                    try:
-                        reaction, user = await self.bot.wait_for(
-                            "reaction_add",
-                            check=lambda r, u: r.message.id == message.id
-                            and r.emoji in QUEUE_EMOJIS
-                            and u == ctx.author,
-                            timeout=60.0,
-                        )
-                    except asyncio.TimeoutError:
-                        await ctx.send("Time is out!")
-                    else:
-                        player = await UserHelper.getOsuUserByName(
-                            make_safe_name(
-                                data["matches"][QUEUE_EMOJIS.index(reaction.emoji)][
-                                    "name"
-                                ]
-                            ),
-                            "info",
-                        )
-                        await message.delete()
-                else:
-                    return await ctx.send("Error! Try again.")
-
-        scores = await UserHelper.getUserScores(
-            player["id"], 0, mods, 5, "best", map_id
-        )
-
-        if len(scores) == 0:
-            return await ctx.send(f"This player has no scores on `{map_id}`!")
-
-        map_fullname = ""
-
-        for idx, score in enumerate(scores):
-            calc = await Calculator.calculate(
-                score["beatmap"]["id"], 0, score["mods"], score["acc"], None
-            )
-            map_fullname = calc["map_fullname"]
-
-            description += (
-                f"""** {idx + 1}. {f' +{Mods(score["mods"])!r}' if score['mods'] != 0 else ''}** [{calc['stars']:.2f}★]\n"""
-                f"▸ {convert_grade_emoji(score['grade'])} ▸ **{score['pp']:.2f}PP**"
-                f"""{f' *({calc["pp"]:.2f}PP for {score["acc"]:.2f}% FC)*' if score['grade'] not in ('S', 'SS', 'X', 'SH') else ''} """
-                f"▸ {score['acc']:.2f}%\n▸ {score['score']} ▸ x{score['max_combo']}/{score['beatmap']['max_combo']} "
-                f"▸ [{score['n300']}/{score['n100']}/{score['n50']}/{score['nmiss']}]\n"
-                f"▸ [Score Set <t:{datetime.fromisoformat(score['play_time']).timestamp().__int__()}:R>]"
-                f"(https://api.sakuru.pw/get_replay?id={score['id']})\n"
-            )
-
-        embed = Embed(color=ctx.author.color, description=description)
-
-        embed.set_author(
-            name=f"Top {len(scores)} Plays for {player['name']} on {map_fullname}",
-            url=f"https://sakuru.pw/u/{player['id']}",
-            icon_url=f"https://sakuru.pw/static/flags/{player['country'].upper()}.png",
-        )
-        embed.set_footer(
-            text="Click on Score Set to download replay.",
-            icon_url="https://sakuru.pw/static/ingame.png",
-        )
-
-        await ctx.send(embed=embed)
 
     @sakuroCommand(hidden=True)
     @commands.check(sakuru_only)
@@ -309,15 +214,16 @@ class AdminCog(commands.Cog, name="Admin"):
                     await emb_mess.delete(delay=15)
                     return
                 else:
-                    reply.content = int(reply.content)
-                    if reply.content > len(bmaps) or reply.content <= 0:
+                    user_reply_parse = int(reply.content)
+
+                    if user_reply_parse > len(bmaps["map"]) or user_reply_parse <= 0:
                         msg = await ctx.send("Specified position is out of range.")
 
                         await reply.delete(delay=15)
                         await msg.delete(delay=15)
                     else:
                         if (
-                            bm_status := bmaps["map"][reply.content - 1]["status"]
+                            bm_status := bmaps["map"][user_reply_parse - 1]["status"]
                         ) == convert_str_status(status):
                             msg = await ctx.send(
                                 f"This map is already {convert_status_str(bm_status)}"
@@ -335,7 +241,7 @@ class AdminCog(commands.Cog, name="Admin"):
                 "secret": config.API_SECRET,
                 "action": "status_map",
                 "admin": admin["safe_name"],
-                "map_id": bmaps["map"][reply.content - 1]["id"],
+                "map_id": bmaps["map"][user_reply_parse - 1]["id"],
                 "map_status": status,
             }
             async with glob.http.post(
